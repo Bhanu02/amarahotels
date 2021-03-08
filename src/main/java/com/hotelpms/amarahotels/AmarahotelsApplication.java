1c12bba654f4ce2fdf21e9cca29197ba849129f4
unknown <bparkash854@gmail.com>
2021-03-08 22:17:21 +0530

    Created model Package

---
+++ src/main/java/com/hotelpms/amarahotels/AmarahotelsApplication.java
@@ -0,0 +1,13 @@
+package com.hotelpms.amarahotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarahotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmarahotelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelpms/amarahotels/AmarahotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelpms.amarahotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmarahotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
